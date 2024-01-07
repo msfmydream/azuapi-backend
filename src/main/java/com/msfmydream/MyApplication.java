1e239925d7a59c11bda5d74a6241f72be593bcc4
msfmydream <msfmydream@gmail.com>
2024-01-07 17:24:54 +0800

    modify: application enable swagger2

--- src/main/java/com/msfmydream/MyApplication.java
+++ src/main/java/com/msfmydream/MyApplication.java
@@ -4,7 +4,9 @@ package com.msfmydream;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 @MapperScan("com.msfmydream.mapper")
 public class MyApplication {

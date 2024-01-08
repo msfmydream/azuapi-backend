4f38c7b9beaa69fb7d09c63ef2ece63b601db5c1
msfmydream <msfmydream@gmail.com>
2024-01-08 23:07:09 +0800

    add: user login service

--- db/mydb.sql
+++ db/mydb.sql
@@ -27,3 +27,20 @@ insert into azuapi.`interface_info` (`name`, `url`, `method`, `description`, `re
 insert into azuapi.`interface_info` (`name`, `url`, `method`, `description`, `requestHeader`, `reponseHeader`, `userId`, `status`) values ('vk', 'www.bruno-weimann.org', 'WCf5J', '5Xk', 'tqP', 'cgI', 2329173, 1);
 insert into azuapi.`interface_info` (`name`, `url`, `method`, `description`, `requestHeader`, `reponseHeader`, `userId`, `status`) values ('A5Wj', 'www.barrett-kuhic.com', 'Jjh', '4rgq', 'j0X', 'E6OAu', 749041, 1);
 insert into azuapi.`interface_info` (`name`, `url`, `method`, `description`, `requestHeader`, `reponseHeader`, `userId`, `status`) values ('15', 'www.karl-shields.net', '2MFIl', 'oTG1', '0KZ', 'OIc', 7, 1);
+
+-- 用户表
+create table if not exists user
+(
+    id           bigint auto_increment comment 'id' primary key,
+    user_name     varchar(256)                           null comment '用户昵称',
+    user_account  varchar(256)                           not null comment '账号',
+    user_avatar   varchar(1024)                          null comment '用户头像',
+    gender       tinyint                                null comment '性别',
+    user_role     varchar(256) default 'user'            not null comment '用户角色：user / admin',
+    user_password varchar(512)                           not null comment '密码',
+    create_time   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint      default 0                 not null comment '是否删除',
+    constraint uni_user_account
+        unique (user_account)
+) comment '用户';
--- src/main/java/com/msfmydream/common/BaseStatus.java
+++ src/main/java/com/msfmydream/common/BaseStatus.java
@@ -5,7 +5,14 @@ public enum BaseStatus implements StatusCode{
     SUCCESS(200,"success"),
     PARAM_ERROR(400,"Invalid parameters"),
     NOT_FOUND(404,"Resource not found"),
-    FAILURE(500,"application internal error");
+    FAILURE(500,"application internal error"),
+
+    NOT_LOGIN_ERROR(40100, "未登录"),
+    NO_AUTH_ERROR(40101, "无权限"),
+
+    OPERATION_ERROR(50002, "操作失败"),
+
+    SYSTEM_ERROR(50001,"system error");
 
 
     private final int code;

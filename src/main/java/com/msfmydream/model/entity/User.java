148f357f043d594cfaef70e0e6d1bc17de4137a3
Azu <msfmydream@163.com>
2024-01-09 22:06:43 +0800

    modify: UserEntity add @tableFile

--- src/main/java/com/msfmydream/model/entity/User.java
+++ src/main/java/com/msfmydream/model/entity/User.java
@@ -24,47 +24,56 @@ public class User implements Serializable {
     /**
      * 用户昵称
      */
+    @TableField(value = "user_name")
     private String userName;
 
     /**
      * 账号
      */
+    @TableField(value = "user_account")
     private String userAccount;
 
     /**
      * 用户头像
      */
+    @TableField(value = "user_avatar")
     private String userAvatar;
 
     /**
      * 性别
      */
+    @TableField(value = "gender")
     private Integer gender;
 
     /**
      * 用户角色: user, admin
      */
+    @TableField(value = "user_role")
     private String userRole;
 
     /**
      * 密码
      */
+    @TableField(value = "user_password")
     private String userPassword;
 
     /**
      * 创建时间
      */
+    @TableField("create_time")
     private Date createTime;
 
     /**
      * 更新时间
      */
+    @TableField("update_time")
     private Date updateTime;
 
     /**
      * 是否删除
      */
     @TableLogic
+    @TableField(value = "is_delete")
     private Integer isDelete;
 
     @TableField(exist = false)
